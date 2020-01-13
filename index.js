93d7561adadca398840f5b0edde85529fc4aeb3d
charlievillacr <charlievilla@gmail.com>
2020-01-13 16:41:22 -0600

    Callbacks y prmesas

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const call = require("./src/call");
 // console.log(call.sync("Carlos Villalobos"));
 
 // Referencia, sin ejecución
-call.withCallback("Dani Guitierrez", call.sync);
+// call.withCallback("Dani Guitierrez", call.sync);
 
-
+// call.withCallback llama a la siguiente función
 // withCallback: function(nombre, cb) {
 //     setTimeout(()=>{
 //        console.log(cb(nombre))
 //     }, 5000);
 // },
+
+
+// En segunda instacia tenemos a las promesas
+// Espera un nombre y va retornarnos una promesa
+// Promesas tienen (resolve, reject)
+
+// console.log(call.withPromise("Promise Withcall"));
+
+call.withPromise("Carlos Villalobos A.")
+    .then(name=>{ console.log(name) });
+
+// withPromise: function(nombre) {
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             resolve(nombre);
+//         }, 5000); 
+//     })
+// }
