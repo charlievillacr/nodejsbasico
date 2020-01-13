158a75ff500278ac2c832312668214978c67978e
charlievillacr <charlievilla@gmail.com>
2020-01-13 16:31:36 -0600

    Callback async

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+// console.log(call.sync("Carlos Villalobos"));
+
+// Referencia, sin ejecución
+call.withCallback("Dani Guitierrez", call.sync);
+
+
+// withCallback: function(nombre, cb) {
+//     setTimeout(()=>{
+//        console.log(cb(nombre))
+//     }, 5000);
+// },
