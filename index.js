9561448262c3fcc10f6fc7e4058b9a9b37b7a089
charlievillacr <charlievilla@gmail.com>
2020-01-13 17:34:28 -0600

    Lectura y escritura de archivos con fs

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs");
+const ops = require("./src/fileops");
+
+let incValue;
+
+// pasamos lectura del file, encoding y callbac
+fs.readFile("./resources/number.txt", "utf8", (err, text)=>{
+    if (err) throw err;
+    console.log(text);
+    const numbers = text.split("\n").map(n=>Number(n));
+    console.log(numbers);
+    // nos devuelve un array
+    // [ '10', '20', '30', '40', '50', '60', '70', '80', '90', '100' ]
+    // Agregamos .map(n=>Number(n)) para que los considere como numeros
+    // [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+    incValue = ops.incrementValues(numbers);
+
+    fs.writeFile("./resources/numbernew.txt", incValue.join("\n"), (err, result)=> {
+        if(err) throw err;
+});
+
+});
