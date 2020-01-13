945f781d6d534355f39fe21bee7abe54d2025d9f
charlievillacr <charlievilla@gmail.com>
2020-01-13 17:08:05 -0600

    Async/Await functions con promesas

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+// Importamos funciones
+
+// Hacemos uso de call.greet
+// call.greet("Carlos Villalobos");
+
+// call.withPromise("Carlos", "Villalobos")
+//     .then(n=>console.log(n));
+
+// Asynch / await
+
+async function callWithPromise() {
+ const fullName =  await call.withPromise("Carlos", "Async/Await")
+        console.log(fullName);
+}
+
+callWithPromise();
