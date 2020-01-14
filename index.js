fa34bd81a9b9a7780a5e910140c09fb526cc59f0
charlievillacr <charlievilla@gmail.com>
2020-01-14 16:30:04 -0600

    Ejecutamos comando y scripts con child_process

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const childProcess = require("child_process");
+
+function execCommand(command){
+    childProcess.exec(command, (err, stdout, stderr)=>{
+        if (err) {
+            console.log(`Error: ${err}`); return;
+        }
+        if (stdout) console.log(`Standard out: \n${stdout}`);
+        if (stderr) console.log(`Standard err: \n${stderr}`);
+    })
+}
+// Ejecutar comando ls
+// execCommand("ls");
+execCommand("node ./src/script --base=5");
