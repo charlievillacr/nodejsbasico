00c6045ced9cba9f219b9c8e45090d03313dab21
charlievillacr <charlievilla@gmail.com>
2020-01-13 18:06:43 -0600

    fs.readFile y fs.writeFile functions

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const fs = require("fs");
 const ops = require("./src/fileops");
 
-let incValue;
 
 // pasamos lectura del file, encoding y callbac
-fs.readFile("./resources/number.txt", "utf8", (err, text)=>{
+fs.readFile("./resources/name.txt", "utf8", (err, text)=>{
     if (err) throw err;
     console.log(text);
-    const numbers = text.split("\n").map(n=>Number(n));
-    console.log(numbers);
+    const names = text.split("\n").map(n=> n.trim());
+    // The trim() method removes whitespace from both ends of a string.
+    const greetingList = ops.callNames(names);
     // nos devuelve un array
     // [ '10', '20', '30', '40', '50', '60', '70', '80', '90', '100' ]
     // Agregamos .map(n=>Number(n)) para que los considere como numeros
     // [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
-    incValue = ops.incrementValues(numbers);
 
-    fs.writeFile("./resources/numbernew.txt", incValue.join("\n"), (err, result)=> {
+    fs.writeFile(
+        "./resources/namesnew.txt", 
+        greetingList.join("\n"), 
+        (err, result)=> {
         if(err) throw err;
-});
+        }
+    );
 
 });
