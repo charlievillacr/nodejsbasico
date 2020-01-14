8b643e33e32f9a4c905d23e72dc7cf63d0bec197
charlievillacr <charlievilla@gmail.com>
2020-01-14 14:42:18 -0600

    Node.js Readline: Introducción de datos por terminal

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const q = [
+    "Cuál es tu primer nombre? ",
+    "Cuál es tu apellido? ",
+    "Cuál es tu edad? "
+];
+
+const AskQuestion = (rl, question) => {
+    return new Promise((resolve, rej)=>{
+        rl.question(question, (answer) =>{
+            resolve(answer);
+        });
+    });
+}
+
+const Ask = (questions) => {
+    return new Promise(async (resolve) => {
+        let resultados = [];
+        for(let index=0; index < questions.length; index++){
+            const result = await AskQuestion(rl, questions[index]);
+            resultados.push(result);
+        };
+        rl.close();
+        resolve(resultados);
+    });
+};
+
+Ask(q)
+ .then(q =>{
+        console.log(`Hola ${q[0]} ${q[1]}, tu edad es la siguiente: ${q[2]} `);
+    });
