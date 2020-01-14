58f7b15ccf1e767be11b21c0d8aaf26855beedc4
charlievillacr <charlievilla@gmail.com>
2020-01-14 17:54:06 -0600

    Node.js Errors: Manejo de errores en Node.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const errors = require("./src/errors");
+const handle = require("./src/handling");
+
+handle.errorFirstCallbackWrong();
+
+// try {
+    // errors.standardErr.range();
+// } catch(err) {
+//     console.log("Ha ocurrido un error: ", err);
+// }
+
+// console.log("Hola");
